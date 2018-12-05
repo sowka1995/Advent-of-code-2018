6bc8acac7628f30b0b6df4c4cb89d484fa56ba3f
Bartosz Sowa <b.sowa9@gmail.com>
2018-12-05 23:26:59 +0100

    Add Day5 Solution

---
+++ Day4/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day4
+{
+    class Program
+    {
+        struct GuardInterval
+        {
+            public DateTime Start { get; set; }
+            public DateTime End { get; set; }
+        }
+
+        class Guard
+        {
+            public int Id { get; set; }
+            public List<GuardInterval> SleepIntervals { get; set; }
+        }
+
+        public static int Day4_Part1(string input)
+        {
+             
+            return -1;
+        }
+
+        static void Main(string[] args)
+        {
+            string filename = Path.Combine(Environment.CurrentDirectory, @"../../input.txt");
+            string input = File.ReadAllText(filename);
+
+            int day4_part1_result = Day4_Part1(input);
+
+            Console.WriteLine("Answer for Day4_Part1: " + day4_part1_result);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Day5/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Text;
+using System.IO;
+
+namespace Day5
+{
+    class Program
+    {
+        public static int Day5_Part1(string input)
+        {
+            StringBuilder sb = new StringBuilder(input);
+
+            for (int i = 0; i < sb.Length - 1; i++)
+            {
+                string left = sb[i].ToString();
+                string rigth = sb[i + 1].ToString();
+                if ((left.ToLower() == rigth && left != rigth) ||
+                    (left.ToUpper() == rigth && left != rigth))
+                {
+                    sb.Remove(i, 2);
+                    i = i > 0 ? i - 2 : i - 1;
+                }
+            }
+
+            return sb.Length;
+        }
+
+        public static int Day5_Part2(string input)
+        {
+            string letters = "ABCDEFGHIKLMNOPQRSTVXYZ";
+          
+            int min = input.Length;
+            foreach (var letter in letters)
+            {
+                StringBuilder sb = new StringBuilder(input);
+                sb.Replace(letter.ToString(), "").Replace(letter.ToString().ToLower(), "");
+
+                for (int i = 0; i < sb.Length - 1; i++)
+                {
+                    string left = sb[i].ToString();
+                    string rigth = sb[i + 1].ToString();
+                    if ((left.ToLower() == rigth && left != rigth) ||
+                        (left.ToUpper() == rigth && left != rigth))
+                    {
+                        sb.Remove(i, 2);
+                        i = i > 0 ? i - 2 : i - 1;
+                    }
+                }
+                if (min > sb.Length)
+                    min = sb.Length;
+            }
+           
+            return min;
+        }
+
+
+        static void Main(string[] args)
+        {
+            string filename = Path.Combine(Environment.CurrentDirectory, @"../../input.txt");
+            string input = File.ReadAllText(filename);
+
+            int day5_part1_result = Day5_Part1(input);
+            int day5_part2_result = Day5_Part2(input);
+
+            Console.WriteLine("Answer for Day4_Part1: " + day5_part1_result);
+            Console.WriteLine("Answer for Day4_Part2: " + day5_part2_result);
+
+            Console.ReadKey();
+        }
+    }
+}
